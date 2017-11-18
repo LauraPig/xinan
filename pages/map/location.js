d313b4b0ccba3522fa6110943f9b6a3bedcc022e
JimmyLiang <liangjianning@xinlebao.com.cn>
2017-11-18 17:21:52 +0800

    添加icon

---
+++ pages/map/location.js
@@ -0,0 +1,94 @@
+// pages/map/location.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  },
+  data: {
+    markers: [{
+      iconPath: "../../img/icon/location.png",
+      id: 0,
+      latitude: 22.664358,
+      longitude: 112.225116,
+      width: 26,
+      height: 26
+    }],
+    polyline: [{
+      points: [{
+        longitude: 112.225116,
+        latitude: 22.664358
+      }, {
+        longitude: 112.225116,
+        latitude: 22.664358
+      }],
+      color: "#FF0000DD",
+      width: 2,
+      dottedLine: true
+    }],
+  },
+  regionchange(e) {
+    console.log(e.type)
+  },
+  markertap(e) {
+    console.log(e.markerId)
+  },
+  controltap(e) {
+    console.log(e.controlId)
+  }
+})
